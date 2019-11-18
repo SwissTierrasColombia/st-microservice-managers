ab9bf7f2d929fa35f7af3bfe551d05131e045826
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-11-18 09:27:41 -0500

    enabled connection with eureka server and config server

--- src/main/java/com/ai/st/microservice/managers/StMicroserviceManagersApplication.java
+++ src/main/java/com/ai/st/microservice/managers/StMicroserviceManagersApplication.java
@@ -2,8 +2,10 @@ package com.ai.st.microservice.managers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StMicroserviceManagersApplication {
 
 	public static void main(String[] args) {
